3ea15a4563df8d76ec391d852c8d3b30dee63588
Rahul <rahulkhanger344@gmail.com>
2026-03-29 18:22:01 +0530

    set cors

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cors({
   origin: [
     'https://new-ecommerce-website-bay.vercel.app',    
-    'https://ecommerce-adminpanel-opal.vercel.app/',   
+    'https://ecommerce-adminpanel-opal.vercel.app',   
     'http://localhost:5173'
   ],
   credentials: true,
